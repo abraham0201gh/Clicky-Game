5c30d90f66ae0b5370a21e40cb8ae6799f9cb963
abraham0201gh <abe1@hotmail.com>
2019-01-14 15:52:12 -0600

    Updates

---
+++ precache-manifest.f2dd97ba195657207922b39dda6b545f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2f9ed21177874c072eef",
+    "url": "/Clicky-Game/static/js/runtime~main.2f9ed211.js"
+  },
+  {
+    "revision": "36d6f528663bec4ec51f",
+    "url": "/Clicky-Game/static/js/main.36d6f528.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/Clicky-Game/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "36d6f528663bec4ec51f",
+    "url": "/Clicky-Game/static/css/main.41e78793.chunk.css"
+  },
+  {
+    "revision": "d922a04a87505480aeec52c29936cb2d",
+    "url": "/Clicky-Game/index.html"
+  }
+];
